c6ed0a840b6ad427a207ec5b8c51b41faad6ed0e
fubuki1128 <fbk1128@gmail.com>
2021-02-12 11:25:19 +0900

    課題の修正
    Task1_4
    課題の修正

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -13,8 +13,8 @@ public class Task1_4 {
 
     // 定数（アカウント情報）
     // ① 登録されている名前とパスワードを定数で定義してください。
-    String name = "alice" ;
-    String pass = "pass" ;
+    boolean alice = true ;
+    boolean pass = true ;
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
     private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
@@ -30,25 +30,25 @@ public class Task1_4 {
     public static void main(String[] args) {
         // 以下の変数「name（名前）」「pass（パスワード）」を使用して、②〜⑤の条件を満たす処理を記述してください。
         // 名前とパスワードは画面からの入力値だと想定してみましょう。
-         boolean name = true ;
-         boolean pass = true ;
+         String name = "alice";
+         String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if( name && pass) {
-            System.out.println("ログイン成功です。") ;
+        if( name == "alice" && pass == "pass" ) {
+            System.out.println(CONST_MSG_SUCCESS) ;
         }
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        else if (name){
-          System.out.println("パスワードに誤りがあります。")  ; 
+        else if (name == "alice"){
+          System.out.println(CONST_MSG_ERROR_PASS)  ; 
         }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        else if (pass){
-          System.out.println("名前に誤りがあります。")  ; 
+        else if (pass == "pass"){
+          System.out.println(CONST_MSG_ERROR_NAME)  ; 
         }
 
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
         else {
-            System.out.println("入力情報に誤りがあります。");
+            System.out.println(CONST_MSG_ERROR_INPUT) ;
         }
     }
 
