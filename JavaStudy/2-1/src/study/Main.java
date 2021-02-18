f12f40b0ec6fd027c80752280484d0eb54f5b8de
fubuki1128 <fbk1128@gmail.com>
2021-02-18 17:15:40 +0900

    2-1課題作成
    2-1課題修正

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -12,8 +12,8 @@ public class Main {
 
       //① firstNameとlastNameという名前の変数を定義し、
       //   firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
-      String firstName = "Fubuki";
-      String lastName = "Oka";
+      String firstName = "Oka";
+      String lastName = "Fubuki";
 
         //getName関数の呼び出しと出力
         System.out.println(getName(firstName, lastName));
@@ -24,16 +24,18 @@ public class Main {
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
         for (int i = 0; i< arr.length; i++) {
+            if  (i % 2 != 0) {
+                System.out.println(i + "は奇数です。");
+            }
+        }
     }
-
     // ② 関数を定義しなさい。
     // 関数名：getName
     // 修飾子：public
     // 引数には①で定義したfirstNameとlastNameを引数で受け取り、
     // last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-    public String getName(String first_Name, String last_Name) {
-        String getName = first_Name + last_Name;
-        return getName ;
+    public static String getName(String first_Name, String last_Name) {
+        return first_Name + last_Name;
     }
 
     // 奇数の判定を行う。
@@ -42,5 +44,4 @@ public class Main {
             System.out.println(num + "は奇数です。");
         }
     }
-}
 }
