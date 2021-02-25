4fa4c4c9f1708c6232ec85d84c6ea53fed504aec
fubuki1128 <fbk1128@gmail.com>
2021-02-25 10:57:39 +0900

    課題提出
    2-1と2-2

--- JavaStudy/2-2/src/study/Main.java
+++ JavaStudy/2-2/src/study/Main.java
@@ -16,38 +16,8 @@ public class Main {
 
         // ① 以下のルールに従いAccountクラスのインスタンスを生成してください。
         // 変数名:「account」
-       Account account = new Account("hoge","123","abc123") ;
+       Account account = new Account("hoge",123,"abc123") ;
         // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
-       System.out.println(account.Account);
+       account.printAccountInfo();
     }
-}
-
-package study;
-
-/**
- * Study.java
- */
-class Account {
-
-    // フィールド変数
-    private String userName;
-    private int id;
-    private String password;
-
-    // コンストラクタ
-    public Account(String userName, int id, String password) {
-        this.userName = userName;
-        this.id = id;
-        this.password = password;
-    }
-
-    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
-    // アクセス修飾子:「protected」
-    // メソッド名:「printAccountInfo」
-    protected void printAccountInfo( ) {
-        System.out.println("ユーザー名は、" + this.userName );
-        System.out.println("IDは、" + this.id );
-        System.out.println("パスワードは、" + this.password );
-    }
-
 }
